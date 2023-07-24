a64c5a1014e530a25fd471e86b257658d0195c6e
jivp-odoo <jivp@odoo.com>
2023-07-24 17:44:18 -0600

    [ADD] GE02 - Map View

---
+++ ge02_team01/__manifest__.py
@@ -0,0 +1,30 @@
+{
+    "name": "GE02 Team 01",
+
+    "summary": "Second group task.",
+
+    "description": """
+This Module is used to create a map view.
+    """,
+
+    "version": "0.1",
+
+    "category": "Kauil/Registry",
+
+    "license": "OPL-1",
+
+    "depends": ["motorcycle_registry", "web_map"],
+
+    "data": [
+        'views/motorcycle_registry_menuitems.xml',
+        'views/motorcycle_registry_views.xml'
+    ],
+
+    "demo": [],
+
+    "author": "team-01",
+
+    "website": "www.odoo.com",
+
+    "installable": True
+}
