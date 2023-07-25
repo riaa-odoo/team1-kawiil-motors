2a1c83fc2e663800b62a9ae0c78e80eecaa85528
riaa-odoo <riaa@odoo.com>
2023-07-25 09:37:29 -0600

    Added basic files

---
+++ ge04_team01/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "GE04 Team 01",
+    "summary": "Fourth group task.",
+    "description": """
+This Module is used to apply $2,500 discounts for new customers.
+    """,
+    "version": "0.1",
+    "category": "Kauil/Registry",
+    "license": "OPL-1",
+    "depends": ["motorcycle_registry"],
+    "data": [],
+    "demo": [],
+    "author": "team-01",
+    "website": "www.odoo.com",
+    "installable": True
+}
