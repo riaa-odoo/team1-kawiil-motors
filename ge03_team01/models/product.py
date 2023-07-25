d0a3debf216335ae94e05fcd0f9345895d8731c8
riaa-odoo <riaa@odoo.com>
2023-07-25 09:14:50 -0600

    Compute now works only for motorcycle products

--- ge03_team01/models/product.py
+++ ge03_team01/models/product.py
@@ -4,7 +4,7 @@ from odoo import api, fields, models
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
-    name = fields.Char("Name", index="trigram", required=True, translate=True, compute="_compute_name", readonly=False)
+    name = fields.Char("Name", index="trigram", translate=True, compute="_compute_name", store=True, readonly=False)
 
     @api.depends("make", "model", "year")
     def _compute_name(self):
@@ -14,4 +14,4 @@ class ProductTemplate(models.Model):
                 if False not in (record.year, record.make, record.model):
                     record.name = f"{record.year} {record.make} {record.model}"
             else:
-                record.name = False
+                record.name = ""
