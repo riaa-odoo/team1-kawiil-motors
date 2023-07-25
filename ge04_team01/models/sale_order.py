5f5015f1d1b5993e7baf8ceaed3a1943c5aa429d
riaa-odoo <riaa@odoo.com>
2023-07-25 16:59:56 -0600

    'Apply discount' button now only appears when is_new_customer == True

--- ge04_team01/models/sale_order.py
+++ ge04_team01/models/sale_order.py
@@ -1,10 +1,13 @@
-from odoo import models
+from odoo import models, fields
 
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
+    is_new_customer = fields.Boolean(related="partner_id.is_new_customer")
+
     def apply_discount(self):
         for sale_order in self:
-            sale_order.pricelist_id = self.env['product.pricelist'].search([('name', '=', 'New Customers')])
-            self.action_update_prices()
+            if sale_order.is_new_customer:
+                sale_order.pricelist_id = self.env['product.pricelist'].search([('name', '=', 'New Customers')])
+                self.action_update_prices()
