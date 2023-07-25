0159d724bce5b7f50495fd4a926947f65b8f29cc
jivp-odoo <jivp@odoo.com>
2023-07-25 15:39:30 -0600

    [EDIT] Task ID in manifes

--- ge02_team01/__manifest__.py
+++ ge02_team01/__manifest__.py
@@ -4,7 +4,7 @@
     "description": """
                     This Module is used to create a map view.
                     Developer: [JIVP]
-                    Task ID: GE2
+                    Task ID: 3426687
                     Link to task: https://www.odoo.com/web#id=3426687&menu_id=4720&cids=17&action=4665&active_id=3426716&model=project.task&view_type=form
                     """,
     "version": "1.0.0",
