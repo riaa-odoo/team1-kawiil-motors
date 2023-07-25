a72b43f8d0401f0f17a7db9b5537d6c049196b97
Corinne Angelica Garcia Alvarez <corinnecoal@Corinnes-MacBook-Air.local>
2023-07-25 15:26:07 -0600

    Added 'Apply Discount' button to sale order view

--- ge04_team01/__manifest__.py
+++ ge04_team01/__manifest__.py
@@ -7,8 +7,9 @@ This Module is used to apply $2,500 discounts for new customers.
     "version": "0.1",
     "category": "Kauil/Registry",
     "license": "OPL-1",
-    "depends": ["motorcycle_registry"],
-    "data": ['data/pricelist_data.xml'],
+    "depends": ["motorcycle_registry", "sale_management"],
+    "data": ['data/pricelist_data.xml',
+             'views/sale_new_customer.xml'],
     "author": "team-01",
     "website": "www.odoo.com",
     "installable": True
