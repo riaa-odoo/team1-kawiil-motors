bf2dd5e55ef827ef54cb4cd48ce4c7bf245cbfc7
riaa-odoo <riaa@odoo.com>
2023-07-25 09:38:51 -0600

    Removed unnecesary comment

--- ge03_team01/models/product.py
+++ ge03_team01/models/product.py
@@ -11,7 +11,6 @@ class ProductTemplate(models.Model):
         for record in self:
             if record.detailed_type is not None and record.detailed_type == "motorcycle" and False not in (
                     record.year, record.make, record.model):
-                # Check if year, make and model fields have been set
                 record.name = f"{record.year} {record.make} {record.model}"
             else:
                 record.name = ""
