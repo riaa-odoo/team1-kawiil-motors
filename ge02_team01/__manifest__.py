9de3baef35b9fd0df4427ad9aba1f62906acdf18
jivp-odoo <jivp@odoo.com>
2023-07-25 09:44:09 -0600

    GE02 - Corrections made

--- ge02_team01/__manifest__.py
+++ ge02_team01/__manifest__.py
@@ -1,30 +1,21 @@
 {
     "name": "GE02 Team 01",
-
     "summary": "Second group task.",
-
     "description": """
-This Module is used to create a map view.
-    """,
-
-    "version": "0.1",
-
-    "category": "Kauil/Registry",
-
+                    This Module is used to create a map view.
+                    Developer: [JIVP]
+                    Task ID: GE2
+                    Link to task: https://www.odoo.com/web#id=3426687&menu_id=4720&cids=17&action=4665&active_id=3426716&model=project.task&view_type=form
+                    """,
+    "version": "1.0.0",
+    "category": "Custom Development",
     "license": "OPL-1",
-
     "depends": ["motorcycle_registry", "web_map"],
-
     "data": [
         'views/motorcycle_registry_menuitems.xml',
         'views/motorcycle_registry_views.xml'
     ],
-
-    "demo": [],
-
     "author": "team-01",
-
     "website": "www.odoo.com",
-
     "installable": True
 }
