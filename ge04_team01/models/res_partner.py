e26c7cd99400a3adf41657458195918966f81725
riaa-odoo <riaa@odoo.com>
2023-07-25 16:11:44 -0600

    Added 'apply_discount' function

--- ge04_team01/models/res_partner.py
+++ ge04_team01/models/res_partner.py
@@ -8,7 +8,6 @@ class Partner(models.Model):
 
     @api.depends("sale_order_ids")
     def _compute_is_new_customer(self):
-        # domain = [("motorcycle", "not in", "sale_order_ids.mapped('order_line').mapped('product_id.detailed_type')")]
         domain = [("product_id.detailed_type", "=", "motorcycle")]
         old_customers = self.env['sale.order.line'].search(domain).mapped('order_id').mapped('partner_id')
         self.is_new_customer = True
