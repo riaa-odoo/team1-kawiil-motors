7525ef5f543af4c7b7c258be20fc3dec28b9d8f0
riaa-odoo <riaa@odoo.com>
2023-07-26 11:24:59 -0600

    Added ge07 folder

---
+++ ge07_team01/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "GE07 Team 01",
+    "summary": "Seventh group task.",
+    "description": """
+Empty Description
+Developer: [DIZR] Diego Zúñiga Rodríguez, [RIAA] Ricardo Alonso, [JIVP] Jordan Vega, [COAL] Corinne Angélica
+Task ID: GE7
+Task URL: https://www.odoo.com/web#id=3427405&cids=17&menu_id=4720&action=4665&active_id=3427418&model=project.task&view_type=form
+    """,
+    "version": "0.1",
+    "category": "Custom Developoment",
+    "license": "OPL-1",
+    "depends": ["motorcycle_registry"],
+    "author": "Odoo Inc",
+    "website": "www.odoo.com",
+    "application": True
+}
